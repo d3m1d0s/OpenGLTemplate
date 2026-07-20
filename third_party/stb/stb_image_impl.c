2408b81ca52e8528e0a6b71bfc74818c2644d266
Demid Ostiakov <demid.ostiakov@gmail.com>
2026-07-20 12:03:39 +0200

    Vendor stb_image with a dedicated implementation unit

---
+++ third_party/stb/stb_image_impl.c
@@ -0,0 +1,4 @@
+/* The single translation unit that carries the stb_image implementation.
+ * Do not define STB_IMAGE_IMPLEMENTATION anywhere else in the project. */
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
